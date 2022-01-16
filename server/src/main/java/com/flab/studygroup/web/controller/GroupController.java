2c69874353a38ba63b623ee74eb746f3fe854c62
oleveloper <oleveloper@gmail.com>
2022-01-16 19:52:16 +0900

    Add CRUD methods

--- server/src/main/java/com/flab/studygroup/web/controller/GroupController.java
+++ server/src/main/java/com/flab/studygroup/web/controller/GroupController.java
@@ -1,6 +1,9 @@
 package com.flab.studygroup.web.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +18,27 @@ import lombok.RequiredArgsConstructor;
 public class GroupController {
 
 	@PostMapping("/creation")
-	public void creation(@RequestBody GroupCreateRequestDto createDto) {
+	public void create(@RequestBody GroupCreateRequestDto createDto) {
 
 	}
 
+	@GetMapping("/{id}")
+	public void findById() {
 
+	}
+
+	@GetMapping("/list")
+	public void findAll() {
+		
+	}
+
+	@PutMapping("/{id}")
+	public void update() {
+
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete () {
+
+	}
 }
