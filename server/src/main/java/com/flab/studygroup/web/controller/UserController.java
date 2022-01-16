c341ea05206f4a187d7154228bb52c2cc74a4f99
oleveloper <oleveloper@gmail.com>
2022-01-16 19:00:29 +0900

    Add Controller and Dto for User and Group

---
+++ server/src/main/java/com/flab/studygroup/web/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.flab.studygroup.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
---
+++ server/src/main/java/com/flab/studygroup/web/dto/GroupResponseDto.java
@@ -0,0 +1,4 @@
+package com.flab.studygroup.web.dto;
+
+public class GroupResponseDto {
+}
