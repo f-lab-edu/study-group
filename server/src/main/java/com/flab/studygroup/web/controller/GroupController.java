e42c0ca39e435fb8fbac6718389007b07e11eb97
oleveloper <oleveloper@gmail.com>
2022-01-16 20:14:52 +0900

    Add PathVariable to RUD methods

--- server/src/main/java/com/flab/studygroup/web/controller/GroupController.java
+++ server/src/main/java/com/flab/studygroup/web/controller/GroupController.java
@@ -2,6 +2,7 @@ package com.flab.studygroup.web.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,22 +24,22 @@ public class GroupController {
 	}
 
 	@GetMapping("/{id}")
-	public void findById() {
+	public void findById(@PathVariable("id") final long id) {
 
 	}
 
 	@GetMapping("/list")
 	public void findAll() {
-		
+
 	}
 
 	@PutMapping("/{id}")
-	public void update() {
+	public void update(@PathVariable("id") final long id) {
 
 	}
 
 	@DeleteMapping("/{id}")
-	public void delete () {
+	public void delete (@PathVariable("id") final long id) {
 
 	}
 }
