fe68b58b5f3d84ae729bf6340a5fc1d701bb1475
oleveloper <oleveloper@gmail.com>
2022-01-16 20:21:50 +0900

    Add annotations

--- server/src/main/java/com/flab/studygroup/web/dto/GroupCreateRequestDto.java
+++ server/src/main/java/com/flab/studygroup/web/dto/GroupCreateRequestDto.java
@@ -1,5 +1,10 @@
 package com.flab.studygroup.web.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class GroupCreateRequestDto {
 	private Long id;
 	private String name;
--- server/src/main/java/com/flab/studygroup/web/dto/UserCreateRequestDto.java
+++ server/src/main/java/com/flab/studygroup/web/dto/UserCreateRequestDto.java
@@ -1,5 +1,10 @@
 package com.flab.studygroup.web.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class UserCreateRequestDto {
 	private Long id;
 	private String name;
