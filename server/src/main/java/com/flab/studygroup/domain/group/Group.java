95ed96e185243e04fa377e02fdc794e78c81f82d
oleveloper <oleveloper@gmail.com>
2022-01-23 22:18:23 +0900

    Create entities of Group and User

---
+++ server/src/main/java/com/flab/studygroup/domain/group/Group.java
@@ -0,0 +1,38 @@
+package com.flab.studygroup.domain.group;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Group {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	private String purpose;
+
+	private String keyword;
+
+	private String day;
+
+	private String time;
+
+	private int online;
+
+	private int limit;
+
+	@Column(name = "START_DATE")
+	private String startdate;
+
+	private String intro;
+}
---
+++ server/src/main/java/com/flab/studygroup/domain/group/GroupRepository.java
@@ -0,0 +1,6 @@
+package com.flab.studygroup.domain.group;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
