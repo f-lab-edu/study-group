875e1c393d90975272032d9855c3f772906a9032
oleveloper <oleveloper@gmail.com>
2022-01-23 22:03:54 +0900

    Move lombokTest to UserCreateRequestDto test

--- server/src/test/java/com/flab/studygroup/web/dto/UserCreateRequestDto.java
+++ server/src/test/java/com/flab/studygroup/web/dto/UserCreateRequestDto.java
@@ -1,4 +1,24 @@
 package com.flab.studygroup.web.dto;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class UserCreateRequestDto {
+	@Test
+	@DisplayName("Lombok Test")
+	public void lombokTest() throws Exception {
+		Long id = 0L;
+		String name = "jane doe";
+		String password = "qwerty1!";
+		String tel = "010-0000-0000";
+
+		GroupCreateRequestDto dto = new GroupCreateRequestDto(id, name, password, tel);
+
+		assertThat(dto.getId()).isEqualTo(id);
+		assertThat(dto.getName()).isEqualTo(name);
+		assertThat(dto.getPassword()).isEqualTo(password);
+		assertThat(dto.getTel()).isEqualTo(tel);
+	}
 }
